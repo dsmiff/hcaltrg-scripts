c2da34fd0b7ecc274766a21872bec533b97eb2e6
Dominic Smith <dosmith@cern.ch>
2016-07-25 13:03:07 +0100

    Initial commits

---
+++ Core/splash.py
@@ -0,0 +1,41 @@
+import random
+version = 'CMSSW_8_1_0_pre8'
+logo = ""
+logo = """                    ____  __ 
+           /     / /    /   |    /
+          / ___ / /    / /| |   /
+         /     / /    / ___ |  /
+        /     / /____/_/  |_/ /___
+                """
+
+
+tagLine = "HCal TP studies"
+toolsVersion = version
+
+def main():
+    print ""
+    printLogo(version)
+    print "\n\t\t\t", tagLine, "\n\n\n"
+    
+    print "    HCal DPG version          :", toolsVersion
+    print "\n"
+
+
+
+def printLogo(version):
+    lightDark = 0
+    if lightDark == 0:
+        lightDark ="3"
+    else:
+        lightDark ="9"
+
+    colour = 2
+    colour = str(int(colour) % 7+1)
+
+    CSI="\x1B[49m\x1B["
+
+    print CSI+lightDark+colour+"m" + logo + CSI + "0m"
+
+
+if __name__ == "__main__":
+    main()
